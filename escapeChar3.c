a424b06f2208d26bfe651e6a27795d5764d65a71
harshitsingh09 <harshit.arvind@gmail.com>
2022-08-28 11:10:55 +0530

    Escape sequences and Comments

---
+++ escapeChar3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("David says, \"Programming is fun!\"\n" "Good day!");
+    printf("**Conditions apply, \"Offer valid until tomorrow\"\n" "C:\\My computer\\My folder\n" "D:/My documents/My file\n" "\\\\Today is Holiday\\\\\n" "This is a triple quoted string\"\"\"A week has 7 days\"\"\"");
+    // single line comment 1
+    // single line comment 2
+    /* multi line comment 1
+    multi line comment 2
+    multi line comment 3*/
+    return 0;
+}
