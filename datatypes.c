65ec6bd8fffbc22a345ebcac4d2f4c7d6e1b48ac
harshitsingh09 <harshit.arvind@gmail.com>
2022-08-30 20:20:26 +0530

    Format specifiers

--- datatypes.c
+++ datatypes.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
-int main(){
-    char letter = 'A' //    Smallest datatype with 1 byte
-    printf(letter)
+int main()
+{
+    unsigned char distA2B = 160;
+    unsigned char distB2C = 40;
+
+    unsigned char distA2C;
+
+    distA2C = distA2B + distB2C;
+
+    printf("The total distance from A to C is %dkm", distA2C);
+    /*
+    Few format specifiers:
+    %x for hexadecimal
+    %c for character
+    %u for unsigned integer
+    %f for float
+    */
+
     return 0;
 }
