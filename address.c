354a2e23ff389024b7f7d377162e5cffc74a3a91
harshitsingh09 <harshit.arvind@gmail.com>
2022-08-30 23:35:46 +0530

    Variable Address

---
+++ address.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char a = 'B';
+    printf("Variable address of 'char a' is: %p\n", &a); //Ampersent '&' sign is used to indicate address of a variable
+    return 0;
+}
