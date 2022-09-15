e4517ee7e92e079b29a27d2f5e164b80773f2c53
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-15 10:33:22 +0530

    static and extern

--- storageClass.c
+++ storageClass.c
@@ -12,7 +12,8 @@ int main(){
 }
 
 void myFunc(void){
-    static int count = 0; // Static makes a variable global but the accessibility is only limited to myFunc()
+    static int count = 0; // Static storage class specifier makes a variable global but the accessibility is only limited to myFunc()
+    // 'extern' storage class specifier allows us to use variables declared globally in another file
     count += 1;
     printf("This function executed %d time(s)\n", count);
 }
