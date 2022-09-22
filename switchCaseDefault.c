eb3af54cbf98927d1df607e4abd2e070a20baaa8
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-22 19:28:12 +0530

    Shape Area Calculator

--- switchCaseDefault.c
+++ switchCaseDefault.c
@@ -16,7 +16,13 @@ int main(){
         printf("Circle Area Calculator\n");
         printf("Enter the radius of Circle:\n");
         scanf("%f", &r);
-        area = 3.14 * r * r;
+        if (r < 0){
+            printf("Radius can not be negative!\n");
+            area = -1;
+        }
+        else {
+            area = 3.14 * r * r;
+        }
         break;
     
     case 't':
@@ -25,14 +31,26 @@ int main(){
         scanf("%f", &h);
         printf("Enter the base of Triangle:\n");
         scanf("%f", &b);
-        area = 0.5 * b * h;
+        if ((h < 0) || (b < 0)){
+            printf("Side can not be negative!\n");
+            area = -1;
+        }
+        else {
+            area = 0.5 * b * h;
+        }
         break;
 
     case 's':
         printf("Square Area Calculator\n");
         printf("Enter the side of square:\n");
         scanf("%f", &h);
-        area = h * h;
+        if (h < 0){
+            printf("Side can not be negative!\n");
+            area = -1;
+        }
+        else {
+            area = h * h;
+        }
         break;
 
     case 'r':
@@ -41,7 +59,13 @@ int main(){
         scanf("%f", &b);
         printf("Enter the height of Rectangle:\n");
         scanf("%f", &h);
-        area = b * h;
+        if ((b < 0) || (h < 0)){
+            printf("Side can not be negative!\n");
+            area = -1;
+        }
+        else {
+            area = b * h;
+        }
         break;
 
     default:
