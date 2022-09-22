58342af2f1780b34d86ef5605928a09d1d1cd10a
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-22 13:01:56 +0530

    Tax amount calculator

--- if_else-if_elseStatements.c
+++ if_else-if_elseStatements.c
@@ -1,21 +1,31 @@
 #include<stdio.h>
 
 void waitForUserInput(void);
+
 int main(void){
-    int num1, num2;
-    printf("Enter first number:\n");
-    scanf("%d", &num1);
-    printf("Enter second number:\n");
-    scanf("%d", &num2);
-    if (num1 == num2){
-        printf("Numbers are EQUAL!\n");
+    int income, tax;
+    double tempIncome;
+
+    printf("Enter total income:\n");
+    scanf("%lf", &tempIncome);
+
+    income = (int)tempIncome;
+
+    if (income < 0){
+        printf("Income cannot be negative!!\n");
+        return 0;
+    }
+    if (income <= 9525){
+        tax = 0;
+    }
+    else if((income > 9525)&&(income <= 38700)){
+        tax = income * 0.12;
+    }
+    else if ((income > 38700)&&(income <= 82500)){
+        tax = income * 0.22;
     }
     else{
-        if (num1 > num2){
-            printf("Num1 is GREATER!");
-        }
-        else{
-            printf("Num2 is GREATER!");
-        }
+        tax = income * 0.32;
     }
+    printf("Tax payable is $%d\n", tax);
 }
