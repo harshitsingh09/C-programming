1de8ee08fa61651a33ddd1f1c4bd5e5eceddfe3f
harshitsingh09 <harshit.arvind@gmail.com>
2022-09-22 12:12:50 +0530

    if else statements

---
+++ if_elseStatement2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+    int num1, num2;
+    printf("Enter first number:\n");
+    scanf("%d", &num1);
+    printf("Enter second number:\n");
+    scanf("%d", &num2);
+    if (num1 == num2){
+        printf("Numbers are EQUAL!\n");
+    }
+    else{  //Nesting if-else statement within else statement
+        if (num1 > num2){
+            printf("Num1 is GREATER!");
+        }
+        else{
+            printf("Num2 is GREATER!");
+        }
+    }
+} //hahahaha
